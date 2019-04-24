3aab3aaacfabf09a9e295acceaa96352b7511953
Kaephas <kkain@mail.greenriver.edu>
2019-04-24 16:50:09 -0400

    add main page with basic formatting; add script file

---
+++ array-object.js
@@ -0,0 +1,8 @@
+/*
+ * Kaephas Kain
+ * 4-24-2019
+ * array-object.js
+ *
+ * Takes an array of varying value types and converts to an object
+ * with fields that store the values of each data type
+ */
