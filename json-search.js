53bfcc801f9fbc4445ad2d722802cc964e648a45
Kaephas <kkain@mail.greenriver.edu>
2019-05-02 00:49:55 -0400

    remove troubleshooting code, add comments

--- json-search.js
+++ json-search.js
@@ -1,7 +1,19 @@
+/*
+ * Kaephas Kain
+ * 5-1-2019
+ * json-search.js
+ *
+ * load json file, compare input to contents, display on web page
+ */
+
+// get elements
 let input = document.getElementById("name");
 let button = document.getElementById("search");
 let output = document.getElementById("display");
+
+// to store people found in json file
 let people = [];
+
 // get all the people from the json file
 let requestURL = "people.json";
 let request = new XMLHttpRequest();
@@ -9,25 +21,26 @@ request.open("GET", requestURL);
 request.responseType = 'json';
 request.send();
 
+// store the request in people
 request.onload = function() {
     people = request.response;
-    // error check values
-    // for(let person of people) {
-    //     console.log(person);
-    //     for(let field in person) if(person.hasOwnProperty(field)){
-    //         console.log(field + ": " + person[field]);
-    //     }
-    // }
 };
 
+// clicking search button
 button.onclick = function() {
+    // get matching names
     let found = checkName();
+    // reset display
     output.innerHTML = "";
+    // if none found
     if(found.length === 0) {
         output.innerHTML = "<p>No person found.</p>";
-    } else {
+    } else {    // if >= 1 found
         for(let person of found) {
+            // paragraph for each person
             output.innerHTML += "<p>";
+            // for each field in person object -- also make sure that person object has the property
+            //                                 -- feels redundant but doesn't run without
             for(let field in person) if (person.hasOwnProperty(field)) {
                 output.innerHTML += field.replace(field[0], field[0].toUpperCase()) + ": " + person[field] + "<br>";
             }
@@ -36,13 +49,14 @@ button.onclick = function() {
     }
 };
 
+// check if search value is in any of the names
 function checkName() {
     let results = [];
+    // compare value to the name field of each person
     for(let person of people) {
-        // if(person.name.toLowerCase().indexOf(input.value.toLowerCase())) {
+        // add to results if string is found in name field
         if(person.name.toLowerCase().includes(input.value.toLowerCase())) {
         results.push(person);
-        console.log("Pushing " + person.name);
         }
     }
     return results;
